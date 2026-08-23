3d63ea0c0986f3f5b3744e49580b1b601cf1c5df
Jitendra <jithu.jitendra.2007@gmail.com>
2026-08-23 21:54:55 +0530

    adding a data structure topic - selection sort

---
+++ selection_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, j, temp, min;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Selection Sort
+    for (i = 0; i < n - 1; i++) {
+        min = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+
+    printf("\nSelection Sort Result:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
